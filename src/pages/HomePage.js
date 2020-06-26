f2e2656eebc5896022ead200de418445df80985b
stevenjchang <stevenjchang@gmail.com>
2020-06-26 16:32:24 -0700

    init create-react-app and tailwindcss

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/tailwind.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>Home Page</div>
+    </>
+  );
+};
+
+export default HomePage;
