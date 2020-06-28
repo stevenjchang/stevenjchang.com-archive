76ca50a41fc8956538058c50ebe3b3629aaadc63
stevenjchang <stevenjchang@gmail.com>
2020-06-27 18:14:20 -0700

    feat(accordian): add basic accordian functionality

--- src/sections/Experience/Experience.js
+++ src/sections/Experience/Experience.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosArrowDropright } from "react-icons/io";
+import classnames from "classnames";
 
 const pageData = [
   {
     jobTitle: "Frontend Engineer",
     company: "Skupos",
-    paragraph: [
+    paragraphs: [
+      {
+        short: "short",
+        medium: "medium",
+        long: "long",
+      },
+      {
+        short: "short",
+        medium: "medium",
+        long: "long",
+      },
       {
         short: "short",
         medium: "medium",
@@ -15,12 +27,45 @@ const pageData = [
 ];
 
 const Card = (props) => {
-  const { jobTitle, company, paragraph } = props;
+  const { jobTitle, company, paragraphs } = props;
   return (
     <div className="c">
       <h3 className="c">
-        {jobTitle} - <span>{company}</span>
+        {jobTitle} @ <span>{company}</span>
       </h3>
+      <BulletDropDowns paragraphs={paragraphs} />
+    </div>
+  );
+};
+
+const BulletDropDowns = ({ paragraphs }) => {
+  if (!paragraphs || !Array.isArray(paragraphs)) {
+    return null;
+  }
+  return (
+    <>
+      {paragraphs.map((data, idx) => (
+        <JobBullet key={idx} index={idx} {...data} />
+      ))}
+    </>
+  );
+};
+
+const JobBullet = ({ short, medium, long, index }) => {
+  const [isOpen, setIsOpen] = useState(index === 0 ? true : false);
+  const cn = classnames("job-upper", { open: isOpen });
+  const cn2 = classnames("job-inner", { open: isOpen });
+  const cn3 = classnames("job-icon", { open: isOpen });
+  return (
+    <div onClick={() => setIsOpen(!isOpen)}>
+      <p className={cn}>
+        <IoIosArrowDropright
+          className={cn3}
+          style={{ display: "inline-block", marginRight: "10px" }}
+        />
+        {short} - <span>{medium}</span>
+      </p>
+      <p className={cn2}>{long}</p>
     </div>
   );
 };
@@ -28,9 +73,11 @@ const Card = (props) => {
 const Experience = () => {
   return (
     <>
-      {pageData.map((data, idx) => (
-        <Card key={idx} {...data} />
-      ))}
+      <div className="container mx-auto my-40 px-24">
+        {pageData.map((data, idx) => (
+          <Card key={idx} {...data} />
+        ))}
+      </div>
     </>
   );
 };
