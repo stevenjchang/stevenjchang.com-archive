8968b49ac0b06896d709e077ed014551d07cf6d0
stevenjchang <stevenjchang@gmail.com>
2020-06-28 20:49:40 -0700

    feat(): portfolio section

---
+++ src/sections/PortfolioSection/PortfolioSection.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const data = [
+  {
+    heading: "Get Tennis Alerts",
+    description:
+      "Never miss another international tennis match. Follow your favorite players and get alerts when tournament matches are announced.",
+    imgUrl: "/images/portfolio_gettennisalerts.png",
+  },
+  {
+    heading: "React / Redux Boilerplate",
+    description:
+      "My simplified boilerplate with customized webpack settings with devServer and proxy. A minimal full-stack javascript boilerplate, with as little configurations as possible. Using React, Node/Express, PostgreSQL, Knex, and Redux",
+    imgUrl: "/images/portfolio_boilerplate.png",
+  },
+  {
+    heading: "Craigslist Search Aggregator",
+    description:
+      "The all in one search aggregator tool for Craigslist. View multiple search results at once, across different keywords and locations.",
+    imgUrl: "/images/portfolio_craigslist.png",
+  },
+  {
+    heading: "SF Crime Data",
+    description:
+      "Reported Crime in SF, in map view. It performs a radial search of variable size around an address provided by the user, and then offers the datapoints (crimes, in this case) for download.",
+    imgUrl: "/images/portfolio_sf_crimedata.png",
+  },
+];
+
+const Card = ({ heading, description, imgUrl }) => {
+  return (
+    <div className="card-container max-w-full flex">
+      <div className="w-1/2 text-center">
+        <h3>{heading}</h3>
+        <h4>{description}</h4>
+      </div>
+      <div className="c w-1/2 max-w-auto h-full border-l border-gray-300">
+        <img className="" src={imgUrl} alt="" />
+      </div>
+    </div>
+  );
+};
+
+const PortfolioSection = () => {
+  return (
+    <>
+      <div id="portfolio-section" className="section-background">
+        <div className="container mx-auto">
+          {data.map((data, idx) => (
+            <Card key={idx} {...data} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PortfolioSection;
