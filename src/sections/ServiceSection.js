daddc7435b33e6b449e0ee384f2506d2033774cc
stevenjchang <stevenjchang@gmail.com>
2021-01-08 16:55:44 -0800

    adjust vertical tabs

--- src/sections/ServiceSection.js
+++ src/sections/ServiceSection.js
@@ -3,75 +3,55 @@ import "sections/ServiceSection.scss";
 export function ServiceSection() {
   return (
     <section id="service-section" className="section">
-      {/* <div className="left"> */}
-      <div class="tabs-container">
-        <label for="tab1" className="tab-label">
-          <div className="tab-label-text">First</div>
-        </label>
-        <label for="tab2" className="tab-label">
-          <div className="tab-label-text">First</div>
-        </label>
-        <label for="tab3" className="tab-label">
-          <div className="tab-label-text">First</div>
-        </label>
-        <label for="tab-n" className="tab-label">
-          <div className="tab-label-text">First</div>
-        </label>
-      </div>
-      {/* </div> */}
-      <div className="right">
-        <input name="tab" id="tab1" type="radio" checked />
-        <div class="tab-content">
-          <div className="tab-content-inner">
-            <h2> Tab Heading </h2>
-            <p>Your peragraph goes here.</p>
-          </div>
-        </div>
-        <input name="tab" id="tab2" type="radio" checked />
-        <div class="tab-content">
-          <div className="tab-content-inner">
-            <h2> Tab Heading </h2>
-            <p>Your peragraph goes here.</p>
-          </div>
-        </div>
-        <input name="tab" id="tab3" type="radio" checked />
-        <div class="tab-content">
-          <div className="tab-content-inner">
-            <h2> Tab Heading </h2>
-            <p>Your peragraph goes here.</p>
-          </div>
-        </div>
-        <input name="tab" id="tab-n" type="radio" checked />
-        <section class="tab-content">
-          <div className="tab-content-inner">
-            <h2> Tab Heading </h2>
-            <p>Your peragraph goes here.</p>
-          </div>
-        </section>
-      </div>
-    </section>
-  );
-}
-
-export function ServiceSection_old() {
-  return (
-    <section id="service-section" className="section">
       <div className="inner-container">
-        <div className="left">
-          <div className="tab active">
-            <p className="">Website</p>
-          </div>
-          <div className="tab">
-            <p className="">E-Commerce site</p>
-          </div>
-          <div className="tab">
-            <p className="">Marketing, Tracking & Analytics</p>
-          </div>
-          <div className="tab">
-            <p className="">Software Development</p>
-          </div>
+        {/* <div className="left"> */}
+        <div class="left tabs-container">
+          <label for="tab1" className="tab-label">
+            <div className="tab-label-text">Website</div>
+          </label>
+          <label for="tab2" className="tab-label">
+            <div className="tab-label-text">E-Commerce site</div>
+          </label>
+          <label for="tab3" className="tab-label">
+            <div className="tab-label-text">
+              Marketing, Tracking & Analytics
+            </div>
+          </label>
+          <label for="tab-n" className="tab-label">
+            <div className="tab-label-text">Software Development</div>
+          </label>
+          {/* </div> */}
         </div>
-        <div className="right">right side</div>
+        <div className="right">
+          <input name="tab" id="tab1" type="radio" checked />
+          <div class="tab-content">
+            <div className="tab-content-inner">
+              <h2>1</h2>
+              <p>Your paragraph goes here.</p>
+            </div>
+          </div>
+          <input name="tab" id="tab2" type="radio" />
+          <div class="tab-content">
+            <div className="tab-content-inner">
+              <h2>2</h2>
+              <p>Your paragraph goes here.</p>
+            </div>
+          </div>
+          <input name="tab" id="tab3" type="radio" />
+          <div class="tab-content">
+            <div className="tab-content-inner">
+              <h2>3</h2>
+              <p>Your paragraph goes here.3</p>
+            </div>
+          </div>
+          <input name="tab" id="tab-n" type="radio" />
+          <section class="tab-content">
+            <div className="tab-content-inner">
+              <h2>4</h2>
+              <p>Your paragraph goes here.</p>
+            </div>
+          </section>
+        </div>
       </div>
     </section>
   );
