e866229766e46cdbfbfdb54aa74133ae1bc4980b
stevenjchang <stevenjchang@gmail.com>
2021-01-12 00:46:01 -0800

    add svg to HeroSection

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -4,21 +4,28 @@ export function HeroSection() {
   return (
     <div id="hero-section-outer" className="">
       <section id="hero-section" className="section-inner">
-        <h1 className="">Software Engineer | Marketing Consultant</h1>
-        <br />
-        <p className="hero-section--description">
-          Based out of San Francisco. I have experience working with early stage
-          startups and small businesses.
+        <div className="left">
+          <h1 className="">Software Engineer &</h1>
+          <h1>Marketing Consultant</h1>
           <br />
+          <p className="hero-section--description">
+            Based out of San Francisco. I have experience working with early
+            stage startups and small businesses.
+            <br />
+            <br />
+            Aside from general development, I can help you get set up with
+            marketing tools, especially with integration and troubleshooting.
+            Have a tracking bug you can't solve? Contact me for a consultation.
+          </p>
           <br />
-          Aside from general development, I can help you get set up with
-          marketing tools, especially with integration and troubleshooting. Have
-          a tracking bug you can't solve? Contact me for a consultation.
-        </p>
-        <br />
-        <div className="">
-          <button className="btn btn-info">view experience</button>
-          <button className="btn btn-secondary">see services</button>
+          <div className="">
+            <button className="btn btn-info">view experience</button>
+            <button className="btn btn-secondary">see services</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src="/undraw_marketing.svg" alt="" className="" />
+          {/* <img src="/undraw_programming.svg" alt="" className="" /> */}
         </div>
       </section>
     </div>
